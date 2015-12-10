2e5b5538a0a95a78d260be98d0f0073c206f5a29
thao nguyeb <roseskindergarten@gmail.com>
2015-12-09 19:01:10 -0500

    add graph

--- app/controllers/clicks_controller.rb
+++ app/controllers/clicks_controller.rb
@@ -3,7 +3,19 @@ class ClicksController < ApplicationController
 
   def index
     @click = Click.new
-    #statistic query
+    @stat = stat
+  end
+
+  def stat
+    query = "select DATE_FORMAT(created_at, '%d'), count(*) from clicks where month(created_at) = 11 group by date(created_at);"
+    result = ActiveRecord::Base.connection.execute(query)
+    result.to_a.map { |x|
+      x[1]
+    }
+  end
+
+  def show
+    render json: stat
   end
 
   def new
