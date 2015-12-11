98b581a31fd9a6854bd7c8c8707bc8a65e32693e
thao nguyeb <roseskindergarten@gmail.com>
2015-12-11 02:17:44 -0500

    fix devise mobile

--- app/assets/javascripts/welcome.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-    screenheight = $(window).height();
-    padding = screenheight/3;
-    $('body').css('padding-top', '100px');
-});
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,8 @@ class WelcomeController < ApplicationController
   def index
     if user_signed_in?
       redirect_to clicks_path
+    else
+      render layout: false
     end
-
-    render layout: false
   end
 end
